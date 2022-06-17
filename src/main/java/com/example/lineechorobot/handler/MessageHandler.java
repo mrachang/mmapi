e7cbb04d1a62a57f9f6d4e95f198315e24dfcbcb
mrachang <46752239+mrachang@users.noreply.github.com>
2022-06-17 17:28:46 +0800

    Update MessageHandler.java

--- src/main/java/com/example/lineechorobot/handler/MessageHandler.java
+++ src/main/java/com/example/lineechorobot/handler/MessageHandler.java
@@ -37,7 +37,7 @@ public class MessageHandler {
 		JSONObject body = new JSONObject();
 		JSONArray messages = new JSONArray();
 		JSONObject message = new JSONObject();
-		message.put("type", "text");
+		message.put("type", "message");
 		message.put("label","回應我");
 		message.put("text", "你好");
 		messages.put(message);
